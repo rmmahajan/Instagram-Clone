5a9e76fee78a920e166e2cf3dd9017ddd8d9dbbf
rmmahajan <manvi.mahajan20@gmail.com>
2020-06-19 22:27:32 +0530

    v3

--- server/app.js
+++ server/app.js
@@ -4,10 +4,6 @@ const PORT = 5000;
 const mongoose = require('mongoose');
 const { URI } = require('./keys');
 
-require('./models/user');
-
-app.use(express.json());
-app.use(require('./routes/auth'));
 
 mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true } );
 mongoose.connection.on('connected', () => {
@@ -16,6 +12,15 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', (err)=>{
     console.log(err);
 });
+
+
+require('./models/user');
+require('./models/post');
+
+
+app.use(express.json());
+app.use(require('./routes/auth'));
+app.use(require('./routes/post'));
 
 
 app.listen(PORT,() => {
