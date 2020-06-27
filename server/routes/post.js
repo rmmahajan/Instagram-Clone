bdc7008992148ba60752024c6fae14091374e92a
rmmahajan <manvi.mahajan20@gmail.com>
2020-06-27 19:55:06 +0530

    v5

--- server/routes/post.js
+++ server/routes/post.js
@@ -39,7 +39,16 @@ router.post('/createpost',requireLogin,(req,res) => {
 });
 
 
-
+router.get('/mypost',requireLogin,(req,res) => {
+    Post.find({postedBy:req.user._id})
+        .populate("PostedBy","_id name")
+        .then(mypost => {
+            res.json({mypost})
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
 
 
 
