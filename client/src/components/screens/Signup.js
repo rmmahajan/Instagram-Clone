dc42b5da9b2ce28fdacb2a40d2311cd8a041883e
rmmahajan <manvi.mahajan20@gmail.com>
2020-07-02 23:16:24 +0530

    v7

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,7 +1,40 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React,{useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import M from 'materialize-css';
 
 const Signup = () => {
+
+    const history = useHistory();
+    const [name,setName] = useState("");
+    const [password,setPassword] = useState("");
+    const [email,setEmail] = useState("");
+
+    const PostData = () => {
+        // if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+        //     M.toast({html: 'Fill data correctly!'});
+        //     return;
+        // }
+        fetch("/signup",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                name,
+                password,
+                email
+            })
+        }).then(res => res.json())
+        .then(data => {
+            if(data.error){
+                M.toast({html: 'Fill the data!'});
+            }
+            else{
+                M.toast({html: 'SignUp Successful!'});
+                history.push('/signin');
+            }
+        })
+    }
 
     return(
         <div className="mycard">
@@ -10,17 +43,25 @@ const Signup = () => {
               <input 
               type="text"
               placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               />
               <input 
               type="text"
               placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               />
               <input 
               type="text"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               />
               
-              <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+              <button className="btn waves-effect waves-light" type="submit" name="action"
+              onClick={()=> PostData()}
+              >Submit
               SignUp
               </button>
               <h6>
