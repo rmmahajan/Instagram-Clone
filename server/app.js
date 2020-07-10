cbf326db527465d98af78de4a058394748d0173a
rmmahajan <manvi.mahajan20@gmail.com>
2020-07-10 22:39:43 +0530

    v9

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
-const { URI } = require('./keys');
+const { MONGOURI } = require('./config/keys');
 
 
-mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true } );
+mongoose.connect(MONGOURI, {useNewUrlParser: true, useUnifiedTopology: true } );
 mongoose.connection.on('connected', () => {
     console.log("Connected");
 });
@@ -24,6 +24,15 @@ app.use(require('./routes/auth'));
 app.use(require('./routes/post'));
 app.use(require('./routes/user'));
 
+
+
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get("*",(req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 
 app.listen(PORT,() => {
--- server/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    URI: "mongodb+srv://manvi:manvi@mern-696u4.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_SECRET: "asdfghjklzxcvbnm"
-}
